06dab24399b81d932435d4916a55f29380eab3fb
Владислав Разинов <razinov63@gmail.com>
2026-05-04 11:47:30 +0300

    v1.0.1

---
+++ app/controllers/feedback_controller.rb
@@ -0,0 +1,82 @@
+class FeedbackController < ApplicationController
+  skip_before_action :check_if_login_required, only: [:vote, :submit]
+  layout 'base'
+  
+  def vote
+    @issue = Issue.find_by(id: params[:id])
+    token = params[:token]
+    
+    if @issue.nil?
+      render_404
+      return
+    end
+    
+    expected_token = Digest::SHA1.hexdigest("#{@issue.id}-#{@issue.created_on}-#{Redmine::Configuration['secret_token']}")
+    
+    if token != expected_token
+      render_404
+      return
+    end
+    
+    custom_field_id = Setting.plugin_redmine_feedback['feedback_custom_field_id']
+    if custom_field_id.present?
+      @existing_feedback = @issue.custom_value_for(custom_field_id)&.value
+    end
+    @existing_comment = Feedback.find_by(issue_id: @issue.id)&.comment
+  end
+  
+  def submit
+    @issue = Issue.find_by(id: params[:id])
+    token = params[:token]
+    
+    expected_token = Digest::SHA1.hexdigest("#{@issue.id}-#{@issue.created_on}-#{Redmine::Configuration['secret_token']}")
+    
+    if @issue.nil? || token != expected_token
+      render_404
+      return
+    end
+    
+    rating = params[:rating]
+    comment = params[:comment]
+    
+    # Сохраняем русское название напрямую
+    rating_value = case rating
+                  when 'good' then 'Хорошо'
+                  when 'okay' then 'Нормально'
+                  when 'bad' then 'Плохо'
+                  else rating
+                  end
+    
+    custom_field_id = Setting.plugin_redmine_feedback['feedback_custom_field_id']
+    if custom_field_id.present? && rating.present?
+      custom_value = @issue.custom_values.detect { |v| v.custom_field_id == custom_field_id.to_i }
+      if custom_value.nil?
+        custom_value = CustomValue.new(
+          customized: @issue,
+          custom_field_id: custom_field_id,
+          value: rating_value
+        )
+        custom_value.save
+      else
+        custom_value.value = rating_value
+        custom_value.save
+      end
+    end
+    
+    if comment.present?
+      feedback = Feedback.find_or_initialize_by(issue_id: @issue.id)
+      feedback.comment = comment
+      feedback.save
+    end
+    
+    if comment.present?
+      journal = @issue.init_journal(User.anonymous, "")
+      journal.notes = "**Комментарий по оценке ТП:**\n\n#{comment}"
+      journal.private_notes = true
+      journal.save
+    end
+    
+    flash[:notice] = 'Спасибо! Ваша оценка сохранена.'
+    redirect_to feedback_vote_path(@issue.id, token: token)
+  end
+end
---
+++ lib/tasks/feedback.rake
@@ -0,0 +1,33 @@
+# redmine_feedback/lib/tasks/feedback.rake
+
+namespace :redmine_feedback do
+  desc 'Create feedback custom field'
+  task create_custom_field: :environment do
+    # Проверяем, существует ли уже поле
+    field = IssueCustomField.find_by(name: 'Оценка поддержки')
+    
+    if field.nil?
+      field = IssueCustomField.new(
+        name: 'Оценка поддержки',
+        field_format: 'string',
+        is_for_all: true,
+        is_filter: true,
+        editable: true,
+        visible: true,
+        trackers: Tracker.all
+      )
+      
+      if field.save
+        # Сохраняем ID поля в настройках плагина
+        Setting.plugin_redmine_feedback = {} unless Setting.plugin_redmine_feedback
+        Setting.plugin_redmine_feedback['feedback_custom_field_id'] = field.id
+        puts "Custom field 'Оценка поддержки' created with ID: #{field.id}"
+      else
+        puts "Error creating custom field: #{field.errors.full_messages.join(', ')}"
+      end
+    else
+      puts "Custom field 'Оценка поддержки' already exists with ID: #{field.id}"
+      Setting.plugin_redmine_feedback['feedback_custom_field_id'] = field.id
+    end
+  end
+end
